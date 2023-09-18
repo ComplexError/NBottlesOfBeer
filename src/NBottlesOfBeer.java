01d09d7cffe16167ebbbdbec1d4ce14cb6073348
TFlynnFRCC <weegeeflower2002@gmail.com>
2023-09-18 17:52:47 -0600

    Initialized BottlesOfBeer repository

---
+++ src/NBottlesOfBeer.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class NBottlesOfBeer {
+    public static void main(String[] args) {
+
+        //initialize Scanner
+        Scanner in = new Scanner(System.in);
+
+        //user prompt and input
+        System.out.println("How many bottles of beer do you wish to start with?");
+        int BottlesOfBeer = in.nextInt();
+
+        //reassigned variable for convenience
+        int i = BottlesOfBeer;
+
+        //introduced while to loop while bottles are above 1
+        while (i > 1) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.\n" +
+                    "Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.\n");
+            i--;
+
+            //last execution of loop function with appropriate grammar
+            if (i == 1) {
+                System.out.println("1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                        "Take one down and pass it around, 0 bottles of beer on the wall.\n");
+            }
+        }
+        //concludes song
+        System.out.println("0 bottles of beer on the wall, 0 bottles of beer.\n" +
+                "Go to the store and buy some more, " + BottlesOfBeer + " bottles of beer on the wall.");
+    }
+}
